b833e6fb2edeb252d13d67221b253c9e0e75123e
Tuljaram Singh <Singht@aimfire.net>
2022-05-30 13:19:50 -0400

    changed return body task1 updated

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@GetMapping("/message")
 	public String message(){
-		return "Spring Boot Test3 Azure Demo - task1 branch. ";
+		return "Spring Boot Test3 Azure Demo - task1 branch -- updated ";
 
 	}
 
